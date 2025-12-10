bbcc45c33a8b28dff6ad3af6bbc7c2b2649e7196
gary <garyhe.05@outlook.com>
2025-12-10 22:04:17 +0800

    finish day02 part2

--- Day02/solution.lisp
+++ Day02/solution.lisp
@@ -1,6 +1,7 @@
 (in-package :adventofcode2025/Day02)
 
 (defparameter +solution-1+ 30608905813)
+(defparameter +solution-2+ 31898925685)
 
 (defun parse-ranges (input)
   (mapcar (lambda (piece)
@@ -10,21 +11,27 @@
                                                  (parse-integer end))))
           (cl-ppcre:split "\\s*,\\s*" input)))
 
-(defun invalid-p (id)
-  (if (cl-ppcre:scan "^(\\d+)\\1$" (write-to-string id))
+(defun invalid-p (id rule)
+  (if (cl-ppcre:scan rule (write-to-string id))
       t
       nil))
 
-(defun parse-range (range)
+(defun parse-range (range rule)
   (let ((start (car range))
         (end   (cdr range)))
     (loop for id from start to end
-          when (invalid-p id)
+          when (invalid-p id rule)
           collect id)))
 
-(defun part-1 ()
+(defun evaluate (pathname rule)
   (reduce #'+
           (mapcan
-            (lambda (range) (parse-range range))
-            (parse-ranges (uiop:read-file-string (input-pathname))))
+            (lambda (range) (parse-range range rule))
+            (parse-ranges (uiop:read-file-string pathname)))
           :initial-value 0))
+
+(defun part-1 ()
+  (evaluate (input-pathname) "^(\\d+)\\1$"))
+
+(defun part-2 ()
+  (evaluate (input-pathname) "^(\\d+)\\1+$"))
